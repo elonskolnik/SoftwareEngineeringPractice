b5dd769dfb6c29f89bc47666b8013eff657a3745
JoshHayden <jhayden@ithaca.edu>
2020-01-23 16:53:44 -0500

    Equivalence classes/border cases added.

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -23,25 +23,30 @@ class BankAccountTest {
 
     @Test
     void isEmailValidTest(){
-        assertTrue(BankAccount.isEmailValid("a@b.com"));
-        assertFalse(BankAccount.isEmailValid(""));
+        assertTrue(BankAccount.isEmailValid("a@b.com")); //This is just a basic correct email
+        assertFalse(BankAccount.isEmailValid("")); //Checks for an empty string
 
-        assertTrue(BankAccount.isEmailValid("a-b@b.com"));
-        assertTrue(BankAccount.isEmailValid("a.b@b.com"));
-        assertTrue(BankAccount.isEmailValid("a_d@b.com"));
-        assertTrue(BankAccount.isEmailValid("a@b.cc"));
-        assertTrue(BankAccount.isEmailValid("a@b-c.com"));
-        assertTrue(BankAccount.isEmailValid("a@b.org"));
+        assertTrue(BankAccount.isEmailValid("a-b@b.com")); //Checks that hyphens are ok as long as they're not right before the @, border case
+        assertTrue(BankAccount.isEmailValid("a.b@b.com")); //Checks that periods before the .com are ok, as long as they aren't right before the @, border case
+        assertTrue(BankAccount.isEmailValid("a_d@b.com")); //Checks that underscores are ok
+        assertTrue(BankAccount.isEmailValid("a@b.cc")); //Checks that having 2 characters after the last period is ok, border case
+        assertTrue(BankAccount.isEmailValid("a@b-c.com"));  //Checks that a hyphen in  domain name is ok
+        assertTrue(BankAccount.isEmailValid("a@b.org")); //Checks that a .org is ok
 
-        assertFalse(BankAccount.isEmailValid("a-@b.com"));
-        assertFalse(BankAccount.isEmailValid("a..b@b.com"));
-        assertFalse(BankAccount.isEmailValid(".a@b.com"));
-        assertFalse(BankAccount.isEmailValid("a#b@b.com"));
-        assertFalse(BankAccount.isEmailValid("a@b.c"));
-        assertFalse(BankAccount.isEmailValid("a@b#c.com"));
-        assertFalse(BankAccount.isEmailValid("a@b"));
-        assertFalse(BankAccount.isEmailValid("a@b..com"));
+        assertFalse(BankAccount.isEmailValid("a-@b.com"));  //Checks for hyphens right before the @, border case
+        assertFalse(BankAccount.isEmailValid("a..b@b.com")); //Checks for multiple periods in a row in username
+        assertFalse(BankAccount.isEmailValid(".a@b.com")); //Checks for a leading  period
+        assertFalse(BankAccount.isEmailValid("a#b@b.com")); //Checks for # in the username
+        assertFalse(BankAccount.isEmailValid("a@b.c")); //Checks for only having one character after the last period, border case
+        assertFalse(BankAccount.isEmailValid("a@b#c.com")); //Checks for # in the server name
+        assertFalse(BankAccount.isEmailValid("a@b")); //Checks for a lack of .com, .org, etc. extension
+        assertFalse(BankAccount.isEmailValid("a@b..com")); //Checks for multiple periods in a row in server name
     }
+    /*
+    The only border case I can think of that's missing is having no or only one character after the  last period,
+    while there is another period in the address, for example, a.b@c.c, I think my current isEmailValid would fail
+    that test, due to indexOf('.') returning  the index of the first period and not the last.
+     */
 
     @Test
     void constructorTest() {
