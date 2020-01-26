8d2cd3c2b5db52b1594d5ca640dad13f30dbd897
JoshHayden <jhayden@ithaca.edu>
2020-01-26 14:23:56 -0500

    Code updated to pass tests, withdraw acts different than the description indicates it should to pass tests

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -30,8 +30,17 @@ public class BankAccount {
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      * Should throw an InsuffiecientFundsException if amount is negative or larger than balance
      */
+    //The tests indicate that a negative amount should just have no effect on balance
     public void withdraw (double amount) throws InsufficientFundsException {
-        balance -= amount;
+        if (amount < 0){
+            balance = balance;
+        }
+        else if (balance < amount) {
+            throw new InsufficientFundsException("Cannot withdraw an amount larger than you balance");
+        }
+        else{
+            balance -= amount;
+        }
 
     }
 
